228898166d47b5a1e9a05d482a59c8d94151f029
Rodrigo <rperin4@gmail.com>
2025-03-31 10:56:17 -0300

    Ajuste para 30 nomes aleatorios

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, send_from_directory
 import redis
 import os
+import random
 from faker import Faker
 
 app = Flask(__name__)
@@ -9,14 +10,23 @@ faker = Faker('pt_BR')
 
 @app.route('/')
 def index():
-    new_name = faker.name()  # Gera um nome aleatório
-    print(f"Gerando nome: {new_name}")  # Log
-    redis_client.lpush('names', new_name)  # Adiciona ao Redis
-    names = redis_client.lrange('names', 0, 9)  # Obtém os últimos 10 nomes
-    count = redis_client.llen('names')  # Conta total de nomes gerados
+    generated_names = [faker.name() for _ in range(30)]
+    
+    if generated_names: 
+        new_name = random.choice(generated_names)
+    else:
+        new_name = "Nome Padrão"
+
+    print(f"Gerando nomes: {generated_names}")
+    print(f"### Nome selecionado para gravação: {new_name}")
+
+    redis_client.lpush('names', new_name)
+    
+    names = redis_client.lrange('names', 0, 9)
+    count = redis_client.llen('names')
+    
     return render_template('index.html', names=names, count=count)
 
-# Rota para servir o CSS diretamente da pasta templates
 @app.route('/style.css')
 def serve_css():
     return send_from_directory(os.path.join(app.root_path, 'templates'), 'style.css')
